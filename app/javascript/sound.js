71ed80f299a3b77521b088907d6689dc658922a6
kakehi22222 <kakehi22222@gmail.com>
2022-12-09 20:10:28 +0900

    音変更

--- app/javascript/sound.js
+++ app/javascript/sound.js
@@ -1,20 +1,22 @@
 function sound() {
   document.querySelector(`.sound_start`).addEventListener('click', () => {
     const gameSound =document.getElementById('geme_sound');
+    const endingSound = document.getElementById("ending_sound");
     gameSound.play();
     gameSound.volume = 0.3;
+    window.setTimeout(function(){
+      if(gameSound.play()){
+        gameSound.pause();
+        endingSound.play();
+        endingSound.volume = 0.3;
+      };
+    }, 10000);
     const ninjas = document.getElementById("ninjas");
       const kenSound = document.getElementById("ken_sound");
       ninjas.addEventListener('click', function(){
         kenSound.play();
         kenSound.volume = 0.3;
         });
-        // const endingSound = document.getElementById("ending_sound");
-        // const resultPoint = document.getElementById("result_point");
-        // resultPoint.addEventListener('click',function(){
-        //   endingSound.play();
-        //   endingSound.volume = 0.3;
-        // })
   });
 };
   if(document.URL.match(/new/)){window.addEventListener('load', sound)};
